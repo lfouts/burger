a22cdbac45679de6dc7673b47ee17dcf4565116d
lfouts <lorenfouts@yahoo.com>
2018-08-25 08:40:52 -0400

    added body parser

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var bodyParser = require("body-parser");
 var app = express();
 app.use(express.static("public"));
 
